1be8477d561f6c85059c5a436753e9ef0df123d2
Hisbonen <admin@hisbonen.org>
2024-09-02 02:00:15 +0000

    Rebuild JSON at f4ee7f1

---
+++ _assets/custom.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contactIcon = document.getElementById('contact-icon');
+
+    // Assuming you're working with the current URL
+    const url = window.location.href;
+
+    // Create a URL object
+    const urlObj = new URL(url);
+
+    // Combine the pathname and the hash into one string for easier manipulation
+    const fullPath = urlObj.pathname + urlObj.hash;
+
+    // Find the last segment after the final '/'
+    const lastSegment = decodeURIComponent(fullPath.substring(fullPath.lastIndexOf('/') + 1));
+
+    // Create the mailto link with the current URL in the subject
+    const email = "submissions@hisbonen.org";
+    const subject = decodeURIComponent("Submission related to: " + lastSegment);
+    const body = decodeURIComponent("Context: " + url) + '%0A%0AYour comment/submission:%0A%0A%0A';
+    
+    // Set the href attribute for the mailto link
+    contactIcon.href = `mailto:${email}?subject=${subject}&body=${body}`;
+    
+    // Open link in a new tab
+    contactIcon.target = "_blank";
+});
